923c264ae312753b3573c0a9ed908f421000f49c
Ngô Vưn Nam <anh02284@gmail.com>
2023-05-27 01:19:42 +0700

    translation

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,36 @@ import { BrowserRouter } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
 import vi_VN from 'antd/locale/vi_VN';
 import App from './App';
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
+
+const resources = {
+    en: {
+        translation: {
+            'Welcome to React': 'Welcome to React and react-i18next',
+        },
+    },
+    vi: {
+        translation: {
+            'Welcome to React': 'Ngo Van Nam react-i18next',
+        },
+    },
+};
+
+i18next.init({
+    resources,
+    lng: 'vi',
+    interpolation: { escapeValue: false }, // React already does escaping
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <ConfigProvider locale={vi_VN}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ConfigProvider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ConfigProvider locale={vi_VN}>
+            <I18nextProvider i18n={i18next}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </I18nextProvider>
+        </ConfigProvider>
+    </React.StrictMode>,
 );
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,13 +1,19 @@
-import { Breadcrumb, Button } from 'antd';
 import { memo } from 'react';
+import { Breadcrumb, Button } from 'antd';
+import i18n from 'i18next';
+import { useTranslation } from 'react-i18next';
 
 const DashBoard = memo(() => {
+    const { t, i18n } = useTranslation();
     return (
         <div>
+            <h1>{t('Welcome to React')}</h1>
             <Breadcrumb style={{ margin: '16px 0' }}>
                 <Breadcrumb.Item>User</Breadcrumb.Item>
                 <Breadcrumb.Item>Bill</Breadcrumb.Item>
             </Breadcrumb>
+            <button onClick={() => i18n.changeLanguage('vi')}>vi</button>
+            <button onClick={() => i18n.changeLanguage('en')}>en</button>
             <div style={{ padding: 24, minHeight: 360 }}>
                 <Button type="primary">Primary Button</Button>
                 <Button type="primary" disabled>
